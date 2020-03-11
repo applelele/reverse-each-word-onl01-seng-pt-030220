58970a3222a621534b9882521949049380f595fe
YUKIKO IKEDA <applelele@gmail.com>
2020-03-11 00:26:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,9 @@ require "pry"
 
 def reverse_each_word(sentence)
   split_array = sentence.split(" ")
+  new_sentence = ""
   split_array.each do |element|
-    element.reverse
+    new_sentence << element.reverse + " "
   end
+  return new_sentence
 end
