875e30dd852a858faf2f533f398ca184ea2a1774
YUKIKO IKEDA <applelele@gmail.com>
2020-03-11 00:50:27 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+require "pry"
+
+def reverse_each_word(sentence)
+  split_array = sentence.split(" ")
+  rev_array = []
+  new_sentence = ""
+  split_array.each do |element|
+     rev_array << element.reverse
+     new_sentence = rev_array.join(" ")
+  end
+  return new_sentence
+end
+
+def reverse_each_word(sentence)
+  split_array = sentence.split(" ")
+  rev_array = split_array.collect {|element| element.reverse}
+  rev_array.join(" ")
+end
